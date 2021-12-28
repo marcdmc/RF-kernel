9b953d8bf975e52f597a5fbf32efdf774d414d70
marcdmc <marc.demiguel.c@gmail.com>
2021-12-28 16:04:35 +0100

    First random forest kernel class

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # RF-kernel
+
+## Installation
+
+### Creating and activating a virtual environment
+
+To create a python virtual environment for the first time use the command:
+```
+python3 -m venv .env
+```
+Then activate the environment and install requirements:
+```
+source .env/bin/activate
+pip install -r requirements.txt
+```
+
+The next time you want to use the environment only activate it using `source .env/bin/activate` and stop it with `deactivate`.
---
+++ tests/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+import sys
+sys.path.insert(0, '../src')
+import RandomForestKernel as rfk
+
+df = pd.read_csv('datasets/Data_for_UCI_named.csv')
+# Select only first 2k rows
+df = df[0:2000]
+df = pd.DataFrame.to_numpy(df)
+# Separate label
+x = df[:, 0:12]
+y = df[:, 13]
+# Split into train-test
+x_training_data, x_test_data, y_training_data, y_test_data = train_test_split(x, y, test_size = 0.2)
+
+r = rfk.RandomForestKernel(x_training_data, y_training_data, x_test_data)
+print(r.K_train)
